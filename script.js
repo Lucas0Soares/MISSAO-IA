4c10a08ce00ba5bb8baccf3893c67201bc4b8325
Lucas0Soares <lucas.soares10@escola.pr.gov.br>
2024-07-04 11:20:25 -0300

    Regulação das Opções e Perguntas

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const caixaPrguntas = document.querySelector(".caixa-perguntas");
 const caixaAternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
 const perguntas =[
     {
         enunciado: "Pergunta 1",
@@ -21,10 +20,8 @@ const perguntas =[
 
     },
 ];
-
 let atual = 0;
 let perguntaAtual;
-
 function mostraPerguntas(){
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
